4768580727423b3da704af5ab630ec03c0f148bd
yangshunxin <15000167782@139.com>
2021-03-23 20:04:45 +0800

    java visual class

---
+++ src/com/ysx/visualClass/Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ysx.visualClass;
+
+/**
+ * @author yangs
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
