e99ff0a18c9df77abee7534239c38b2267236c8d
yangshunxin <15000167782@139.com>
2021-06-27 15:55:08 +0800

    add marven

---
+++ src/marven/MarvenLog.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package marven;
+
+/**
+ * @author yangs
+ * Marven:
+ * 	用途：方便jar包导入
+ * 	核心思想：约定大于配置
+ * 		有约束，不要去违反；
+ * 	Maven会规定好你该如何去编写我们的java代码（目录结果），必须按照这个规范来。
+ * 	问题：
+ * 		Maven3.6.2有问题，降为3.6.1
+ * 		
+ * 
+ *
+ */
+public class MarvenLog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
